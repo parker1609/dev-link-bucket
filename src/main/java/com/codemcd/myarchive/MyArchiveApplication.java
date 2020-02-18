d2b5f3e6782becaaeb7407c41661967d279aa656
CODEMCD <psbum1609@gmail.com>
2020-02-18 11:47:23 +0900

    init project

---
+++ src/main/java/com/codemcd/myarchive/MyArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.codemcd.myarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codemcd/myarchive/MyArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemcd.myarchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
