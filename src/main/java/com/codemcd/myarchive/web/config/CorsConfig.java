83c295eb924d7735a0c49f689a7f925f0f5ac20b
CODEMCD <psbum1609@gmail.com>
2020-02-25 15:32:24 +0900

    chore: CORS 설정 추가
    * npm run serve로 동작하는 Vue.js 서버에서 Spring Boot 서버로 테스트하기 위해 CORS 설정함.
    
    * 가장 간단한 CORS 설정으로 추가했음.

---
+++ src/main/java/com/codemcd/myarchive/web/config/CorsConfig.java
@@ -0,0 +1,14 @@
+package com.codemcd.myarchive.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+    }
+}
